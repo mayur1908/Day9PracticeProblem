48458a741b5151f518871abcaa425f6960dad2ff
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-08 15:36:25 +0530

    [Wlc On Adress Book ]

---
+++ DayNinePracticeProb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DayNinePracticeProb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Create employee instances for different companies
+            Employee company1Employee = new Employee();
+            Employee company2Employee = new Employee();
+
+            // Calculate wages for Company 1
+            int company1Wage = company1Employee.CalculateWage(25, 22, 120);
+            Console.WriteLine("Company 1 Total Wage: " + company1Wage);
+
+            // Calculate wages for Company 2
+            int company2Wage = company2Employee.CalculateWage(30, 20, 100);
+            Console.WriteLine("Company 2 Total Wage: " + company2Wage);
+            */
+            Console.WriteLine("WellCome to Adress Book ");
+
+            Console.ReadLine();
+        }
+    }
+}
